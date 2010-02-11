94c219d2172721406126e13ad52734d00bc8cf4a
Ask Solem <askh@opera.com>
2010-02-11 15:30:24 +0100

    Initial commit

---
+++ dispatch/__init__.py
@@ -0,0 +1,15 @@
+"""Multi-consumer multi-producer dispatching mechanism"""
+
+from dispatch.dispatcher import Signal
+
+VERSION = (3, 0, 0)
+
+__version__ = ".".join(map(str, VERSION))
+__author__ = "Patrick K. O'Brien"
+__contact__ = "askh@opera.com"
+__homepage__ = "http://github.com/ask/dispatch/"
+__docformat__ = "restructuredtext"
+__license__ = "BSD"
+
+
+
---
+++ setup.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import codecs
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+from distutils.command.install_data import install_data
+from distutils.command.install import INSTALL_SCHEMES
+import sys
+
+import dispatch
+
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+src_dir = "dispatch"
+
+
+def osx_install_data(install_data):
+
+    def finalize_options(self):
+        self.set_undefined_options("install", ("install_lib", "install_dir"))
+        install_data.finalize_options(self)
+
+
+def fullsplit(path, result=None):
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+for dirpath, dirnames, filenames in os.walk(src_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith("."):
+            del dirnames[i]
+    for filename in filenames:
+        if filename.endswith(".py"):
+            packages.append('.'.join(fullsplit(dirpath)))
+        else:
+            data_files.append([dirpath, [os.path.join(dirpath, f) for f in
+                filenames]])
+
+if os.path.exists("README.rst"):
+    long_description = codecs.open('README.rst', "r", "utf-8").read()
+else:
+    long_description = "See http://pypi.python.org/pypi/dispatch"
+
+setup(
+    name='dispatch',
+    version=dispatch.__version__,
+    description=dispatch.__doc__,
+    author=dispatch.__author__,
+    author_email=dispatch.__contact__,
+    url=dispatch.__homepage__,
+    platforms=["any"],
+    packages=packages,
+    data_files=data_files,
+    zip_safe=False,
+    test_suite="nose.collector",
+    install_requires=[
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    long_description=long_description,
+)
